f308edb8c15bcaf8e53157fd855a92cea15fe364
Proctored Games <proctoredgames@hotmail.com>
2026-02-03 21:03:01 -0800

    tile block now renders correctly

--- src/main/java/com/proctoredgames/tiled/block/entity/renderer/TiledModelLoadingPlugin.java
+++ src/main/java/com/proctoredgames/tiled/block/entity/renderer/TiledModelLoadingPlugin.java
@@ -10,6 +10,7 @@ import net.minecraft.util.Identifier;
 @Environment(EnvType.CLIENT)
 public class TiledModelLoadingPlugin implements ModelLoadingPlugin {
     public static final ModelIdentifier TILE_BLOCK_MODEL = new ModelIdentifier(Identifier.of(Tiled.MOD_ID, "tile_block"), "");
+    public static final ModelIdentifier TILE_BLOCK_MODEL_ITEM = new ModelIdentifier(Identifier.of(Tiled.MOD_ID, "tile_block"), "inventory");
 
     @Override
     public void onInitializeModelLoader(Context pluginContext) {
@@ -17,7 +18,7 @@ public class TiledModelLoadingPlugin implements ModelLoadingPlugin {
         pluginContext.modifyModelOnLoad().register((original, context) -> {
             // This is called for every model that is loaded, so make sure we only target ours
             final ModelIdentifier id = context.topLevelId();
-            if(id != null && id.equals(TILE_BLOCK_MODEL)) {
+            if (id != null && (id.equals(TILE_BLOCK_MODEL) || id.equals(TILE_BLOCK_MODEL_ITEM))) {
                 return new TileBlockModel();
             } else {
                 // If we don't modify the model we just return the original as-is
