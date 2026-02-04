1f725e5513e82c4d6ec147b795e933657f7b99ea
Proctored Games <proctoredgames@hotmail.com>
2026-02-04 00:03:51 -0800

    ugh

--- src/main/java/com/proctoredgames/tiled/block/entity/renderer/TileBlockModel.java
+++ src/main/java/com/proctoredgames/tiled/block/entity/renderer/TileBlockModel.java
@@ -7,9 +7,7 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.renderer.v1.Renderer;
 import net.fabricmc.fabric.api.renderer.v1.RendererAccess;
-import net.fabricmc.fabric.api.renderer.v1.mesh.Mesh;
 import net.fabricmc.fabric.api.renderer.v1.mesh.MeshBuilder;
-import net.fabricmc.fabric.api.renderer.v1.mesh.MutableQuadView;
 import net.fabricmc.fabric.api.renderer.v1.mesh.QuadEmitter;
 import net.fabricmc.fabric.api.renderer.v1.model.FabricBakedModel;
 import net.fabricmc.fabric.api.renderer.v1.model.ModelHelper;
@@ -20,6 +18,8 @@ import net.minecraft.client.render.model.json.ModelOverrideList;
 import net.minecraft.client.render.model.json.ModelTransformation;
 import net.minecraft.client.texture.Sprite;
 import net.minecraft.client.util.SpriteIdentifier;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.screen.PlayerScreenHandler;
 import net.minecraft.util.Identifier;
@@ -30,11 +30,13 @@ import net.minecraft.world.BlockRenderView;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
 @Environment(EnvType.CLIENT)
 public class TileBlockModel implements UnbakedModel, BakedModel, FabricBakedModel {
+
     private static final SpriteIdentifier[] SPRITE_IDS = new SpriteIdentifier[]{
             new SpriteIdentifier(PlayerScreenHandler.BLOCK_ATLAS_TEXTURE, Identifier.of(Tiled.MOD_ID, "block/black_tiles")),
             new SpriteIdentifier(PlayerScreenHandler.BLOCK_ATLAS_TEXTURE, Identifier.of(Tiled.MOD_ID, "block/blue_tiles")),
@@ -56,74 +58,97 @@ public class TileBlockModel implements UnbakedModel, BakedModel, FabricBakedMode
 
     private final Sprite[] sprites = new Sprite[SPRITE_IDS.length];
 
-    // Some constants to avoid magic numbers, these need to match the SPRITE_IDS
-    private static final int SPRITE_SIDE = 0;
-    private static final int SPRITE_TOP = 1;
-
-    private Mesh mesh;
-
     @Override
     public Collection<Identifier> getModelDependencies() {
-        return List.of(); // This model does not depend on other models.
+        return List.of(); // No other models
     }
 
     @Override
     public void setParents(Function<Identifier, UnbakedModel> modelLoader) {
-        // This is related to model parents, it's not required for our use case
+        // Not used
     }
-
 
     @Override
     public BakedModel bake(Baker baker, Function<SpriteIdentifier, Sprite> textureGetter, ModelBakeSettings rotationContainer) {
-        // Get the sprites
-        for(int i = 0; i < SPRITE_IDS.length; ++i) {
+        // Load sprites once
+        for (int i = 0; i < SPRITE_IDS.length; i++) {
             sprites[i] = textureGetter.apply(SPRITE_IDS[i]);
         }
-        // Build the mesh using the Renderer API
-        Renderer renderer = RendererAccess.INSTANCE.getRenderer();
-        MeshBuilder builder = renderer.meshBuilder();
-        QuadEmitter emitter = builder.getEmitter();
-
-        Sprite topLeft = sprites[1];
-        Sprite topRight = sprites[5];
-        Sprite bottomLeft = sprites[10];
-        Sprite bottomRight = sprites[8];
-
-        for(Direction direction : Direction.values()) {
-            emitter.square(direction, 0f, 0f, 0.5f, 0.5f, 0.0f);
-            emitter.spriteBake(topLeft, MutableQuadView.BAKE_LOCK_UV);
-            emitter.color(-1, -1, -1, -1);
-            emitter.emit();
-
-            emitter.square(direction, 0.5f, 0f, 1f, 0.5f, 0.0f);
-            emitter.spriteBake(topRight, MutableQuadView.BAKE_LOCK_UV);
-            emitter.color(-1, -1, -1, -1);
-            emitter.emit();
-
-            emitter.square(direction, 0f, 0.5f, 0.5f, 1f, 0.0f);
-            emitter.spriteBake(bottomLeft, MutableQuadView.BAKE_LOCK_UV);
-            emitter.color(-1, -1, -1, -1);
-            emitter.emit();
-
-            emitter.square(direction, 0.5f, 0.5f, 1f, 1f, 0.0f);
-            emitter.spriteBake(bottomRight, MutableQuadView.BAKE_LOCK_UV);
-            emitter.color(-1, -1, -1, -1);
-            emitter.emit();
-        }
-        mesh = builder.build();
-
         return this;
     }
 
     @Override
+    public void emitBlockQuads(BlockRenderView world, BlockState state, BlockPos pos,
+                               Supplier<Random> randomSupplier, RenderContext context) {
+        TileBlockBE be = (TileBlockBE) world.getBlockEntity(pos);
+        Tiles tiles = be != null ? be.getTiles() : Tiles.DEFAULT;
+
+        Renderer renderer = RendererAccess.INSTANCE.getRenderer();
+        MeshBuilder builder = renderer.meshBuilder();
+        QuadEmitter emitter = builder.getEmitter();
+
+        Sprite topLeft = sprites[getTextureIdFromTile(tiles.top_left())];
+        Sprite topRight = sprites[getTextureIdFromTile(tiles.top_right())];
+        Sprite bottomLeft = sprites[getTextureIdFromTile(tiles.bottom_left())];
+        Sprite bottomRight = sprites[getTextureIdFromTile(tiles.bottom_right())];
+
+        for (Direction dir : Direction.values()) {
+            emitter.square(dir, 0f, 0f, 0.5f, 0.5f, 0f);
+            emitter.spriteBake(topLeft, QuadEmitter.BAKE_LOCK_UV);
+            emitter.color(-1, -1, -1, -1);
+            emitter.emit();
+
+            emitter.square(dir, 0.5f, 0f, 1f, 0.5f, 0f);
+            emitter.spriteBake(topRight, QuadEmitter.BAKE_LOCK_UV);
+            emitter.color(-1, -1, -1, -1);
+            emitter.emit();
+
+            emitter.square(dir, 0f, 0.5f, 0.5f, 1f, 0f);
+            emitter.spriteBake(bottomLeft, QuadEmitter.BAKE_LOCK_UV);
+            emitter.color(-1, -1, -1, -1);
+            emitter.emit();
+
+            emitter.square(dir, 0.5f, 0.5f, 1f, 1f, 0f);
+            emitter.spriteBake(bottomRight, QuadEmitter.BAKE_LOCK_UV);
+            emitter.color(-1, -1, -1, -1);
+            emitter.emit();
+        }
+
+        // Output dynamically generated mesh
+        builder.build().outputTo(context.getEmitter());
+    }
+
+    private int getTextureIdFromTile(Optional<Item> tile) {
+        if (tile == null || tile.isEmpty()) return 0;
+
+        Item item = tile.get();
+        if (item == Items.BLACK_CONCRETE) return 0;
+        if (item == Items.BLUE_CONCRETE) return 1;
+        if (item == Items.BROWN_CONCRETE) return 2;
+        if (item == Items.CYAN_CONCRETE) return 3;
+        if (item == Items.GRAY_CONCRETE) return 4;
+        if (item == Items.GREEN_CONCRETE) return 5;
+        if (item == Items.LIGHT_BLUE_CONCRETE) return 6;
+        if (item == Items.LIGHT_GRAY_CONCRETE) return 7;
+        if (item == Items.LIME_CONCRETE) return 8;
+        if (item == Items.MAGENTA_CONCRETE) return 9;
+        if (item == Items.ORANGE_CONCRETE) return 10;
+        if (item == Items.PINK_CONCRETE) return 11;
+        if (item == Items.PURPLE_CONCRETE) return 12;
+        if (item == Items.RED_CONCRETE) return 13;
+        if (item == Items.WHITE_CONCRETE) return 14;
+        if (item == Items.YELLOW_CONCRETE) return 15;
+        return 0; // fallback
+    }
+
+    @Override
     public List<BakedQuad> getQuads(BlockState state, Direction face, Random random) {
-        // Don't need because we use FabricBakedModel instead. However, it's better to not return null in case some mod decides to call this function.
-        return List.of();
+        return List.of(); // Not used, FabricBakedModel handles dynamic quads
     }
 
     @Override
     public boolean useAmbientOcclusion() {
-        return true; // we want the block to have a shadow depending on the adjacent blocks
+        return true;
     }
 
     @Override
@@ -138,24 +163,14 @@ public class TileBlockModel implements UnbakedModel, BakedModel, FabricBakedMode
 
     @Override
     public Sprite getParticleSprite() {
-        return sprites[SPRITE_TOP]; // Block break particle, let's use furnace_top
+        return sprites[0]; // fallback black
     }
 
     @Override
     public boolean isVanillaAdapter() {
-        return false; // False to trigger FabricBakedModel rendering
+        return false;
     }
 
-    @Override
-    public void emitBlockQuads(BlockRenderView blockRenderView, BlockState blockState, BlockPos blockPos, Supplier<Random> supplier, RenderContext renderContext) {
-        // Render function
-        TileBlockBE be = (TileBlockBE) blockRenderView.getBlockEntity(blockPos);
-        Tiles tiles = be != null ? be.getTiles() : Tiles.DEFAULT;
-        // We just render the mesh
-        mesh.outputTo(renderContext.getEmitter());
-    }
-
-    // We need to implement getTransformation() and getOverrides()
     @Override
     public ModelTransformation getTransformation() {
         return ModelHelper.MODEL_TRANSFORM_BLOCK;
@@ -166,13 +181,11 @@ public class TileBlockModel implements UnbakedModel, BakedModel, FabricBakedMode
         return ModelOverrideList.EMPTY;
     }
 
-    // We will also implement this method to have the correct lighting in the item rendering. Try to set this to false and you will see the difference.
     @Override
     public boolean isSideLit() {
         return true;
     }
 
-    // Finally, we can implement the item render function
     @Override
     public void emitItemQuads(ItemStack itemStack, Supplier<Random> supplier, RenderContext renderContext) {
         mesh.outputTo(renderContext.getEmitter());
