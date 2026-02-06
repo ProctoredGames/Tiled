6e067a2d4153b96f77022d7d36bd8c661b31bf15
Proctored Games <proctoredgames@hotmail.com>
2026-02-05 17:59:14 -0800

    tile blocks are now properly mineable

--- src/main/java/com/proctoredgames/tiled/TiledDataGenerator.java
+++ src/main/java/com/proctoredgames/tiled/TiledDataGenerator.java
@@ -1,11 +1,14 @@
 package com.proctoredgames.tiled;
 
+import com.proctoredgames.tiled.datagen.ModBlockTagProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
 public class TiledDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
+		pack.addProvider(ModBlockTagProvider::new);
 	}
 }
