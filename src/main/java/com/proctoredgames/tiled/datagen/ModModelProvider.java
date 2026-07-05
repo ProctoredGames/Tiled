802d885e3ab8f2195bfc3946efdfbadde2e32733
Proctored Games <proctoredgames@hotmail.com>
2026-07-05 00:06:24 -0700

    1.2.0

--- src/main/java/com/proctoredgames/tiled/datagen/ModModelProvider.java
+++ src/main/java/com/proctoredgames/tiled/datagen/ModModelProvider.java
@@ -17,39 +17,39 @@ public class ModModelProvider extends FabricModelProvider {
 
     @Override
     public void generateItemModels(ItemModelGenerator itemModelGenerator) {
-        itemModelGenerator.register(ModItems.SMALL_WHITE_TILE, Models.GENERATED);
-        itemModelGenerator.register(ModItems.SMALL_LIGHT_GRAY_TILE, Models.GENERATED);
-        itemModelGenerator.register(ModItems.SMALL_GRAY_TILE, Models.GENERATED);
-        itemModelGenerator.register(ModItems.SMALL_BLACK_TILE, Models.GENERATED);
-        itemModelGenerator.register(ModItems.SMALL_BROWN_TILE, Models.GENERATED);
-        itemModelGenerator.register(ModItems.SMALL_RED_TILE, Models.GENERATED);
-        itemModelGenerator.register(ModItems.SMALL_ORANGE_TILE, Models.GENERATED);
-        itemModelGenerator.register(ModItems.SMALL_YELLOW_TILE, Models.GENERATED);
-        itemModelGenerator.register(ModItems.SMALL_LIME_TILE, Models.GENERATED);
-        itemModelGenerator.register(ModItems.SMALL_GREEN_TILE, Models.GENERATED);
-        itemModelGenerator.register(ModItems.SMALL_CYAN_TILE, Models.GENERATED);
-        itemModelGenerator.register(ModItems.SMALL_LIGHT_BLUE_TILE, Models.GENERATED);
-        itemModelGenerator.register(ModItems.SMALL_BLUE_TILE, Models.GENERATED);
-        itemModelGenerator.register(ModItems.SMALL_PURPLE_TILE, Models.GENERATED);
-        itemModelGenerator.register(ModItems.SMALL_MAGENTA_TILE, Models.GENERATED);
-        itemModelGenerator.register(ModItems.SMALL_PINK_TILE, Models.GENERATED);
+        itemModelGenerator.register(ModItems.SMALL_WHITE_CONCRETE_TILE, Models.GENERATED);
+        itemModelGenerator.register(ModItems.SMALL_LIGHT_GRAY_CONCRETE_TILE, Models.GENERATED);
+        itemModelGenerator.register(ModItems.SMALL_GRAY_CONCRETE_TILE, Models.GENERATED);
+        itemModelGenerator.register(ModItems.SMALL_BLACK_CONCRETE_TILE, Models.GENERATED);
+        itemModelGenerator.register(ModItems.SMALL_BROWN_CONCRETE_TILE, Models.GENERATED);
+        itemModelGenerator.register(ModItems.SMALL_RED_CONCRETE_TILE, Models.GENERATED);
+        itemModelGenerator.register(ModItems.SMALL_ORANGE_CONCRETE_TILE, Models.GENERATED);
+        itemModelGenerator.register(ModItems.SMALL_YELLOW_CONCRETE_TILE, Models.GENERATED);
+        itemModelGenerator.register(ModItems.SMALL_LIME_CONCRETE_TILE, Models.GENERATED);
+        itemModelGenerator.register(ModItems.SMALL_GREEN_CONCRETE_TILE, Models.GENERATED);
+        itemModelGenerator.register(ModItems.SMALL_CYAN_CONCRETE_TILE, Models.GENERATED);
+        itemModelGenerator.register(ModItems.SMALL_LIGHT_BLUE_CONCRETE_TILE, Models.GENERATED);
+        itemModelGenerator.register(ModItems.SMALL_BLUE_CONCRETE_TILE, Models.GENERATED);
+        itemModelGenerator.register(ModItems.SMALL_PURPLE_CONCRETE_TILE, Models.GENERATED);
+        itemModelGenerator.register(ModItems.SMALL_MAGENTA_CONCRETE_TILE, Models.GENERATED);
+        itemModelGenerator.register(ModItems.SMALL_PINK_CONCRETE_TILE, Models.GENERATED);
 
-        itemModelGenerator.register(ModItems.WHITE_TILE, Models.GENERATED);
-        itemModelGenerator.register(ModItems.LIGHT_GRAY_TILE, Models.GENERATED);
-        itemModelGenerator.register(ModItems.GRAY_TILE, Models.GENERATED);
-        itemModelGenerator.register(ModItems.BLACK_TILE, Models.GENERATED);
-        itemModelGenerator.register(ModItems.BROWN_TILE, Models.GENERATED);
-        itemModelGenerator.register(ModItems.RED_TILE, Models.GENERATED);
-        itemModelGenerator.register(ModItems.ORANGE_TILE, Models.GENERATED);
-        itemModelGenerator.register(ModItems.YELLOW_TILE, Models.GENERATED);
-        itemModelGenerator.register(ModItems.LIME_TILE, Models.GENERATED);
-        itemModelGenerator.register(ModItems.GREEN_TILE, Models.GENERATED);
-        itemModelGenerator.register(ModItems.CYAN_TILE, Models.GENERATED);
-        itemModelGenerator.register(ModItems.LIGHT_BLUE_TILE, Models.GENERATED);
-        itemModelGenerator.register(ModItems.BLUE_TILE, Models.GENERATED);
-        itemModelGenerator.register(ModItems.PURPLE_TILE, Models.GENERATED);
-        itemModelGenerator.register(ModItems.MAGENTA_TILE, Models.GENERATED);
-        itemModelGenerator.register(ModItems.PINK_TILE, Models.GENERATED);
+        itemModelGenerator.register(ModItems.WHITE_CONCRETE_TILE, Models.GENERATED);
+        itemModelGenerator.register(ModItems.LIGHT_GRAY_CONCRETE_TILE, Models.GENERATED);
+        itemModelGenerator.register(ModItems.GRAY_CONCRETE_TILE, Models.GENERATED);
+        itemModelGenerator.register(ModItems.BLACK_CONCRETE_TILE, Models.GENERATED);
+        itemModelGenerator.register(ModItems.BROWN_CONCRETE_TILE, Models.GENERATED);
+        itemModelGenerator.register(ModItems.RED_CONCRETE_TILE, Models.GENERATED);
+        itemModelGenerator.register(ModItems.ORANGE_CONCRETE_TILE, Models.GENERATED);
+        itemModelGenerator.register(ModItems.YELLOW_CONCRETE_TILE, Models.GENERATED);
+        itemModelGenerator.register(ModItems.LIME_CONCRETE_TILE, Models.GENERATED);
+        itemModelGenerator.register(ModItems.GREEN_CONCRETE_TILE, Models.GENERATED);
+        itemModelGenerator.register(ModItems.CYAN_CONCRETE_TILE, Models.GENERATED);
+        itemModelGenerator.register(ModItems.LIGHT_BLUE_CONCRETE_TILE, Models.GENERATED);
+        itemModelGenerator.register(ModItems.BLUE_CONCRETE_TILE, Models.GENERATED);
+        itemModelGenerator.register(ModItems.PURPLE_CONCRETE_TILE, Models.GENERATED);
+        itemModelGenerator.register(ModItems.MAGENTA_CONCRETE_TILE, Models.GENERATED);
+        itemModelGenerator.register(ModItems.PINK_CONCRETE_TILE, Models.GENERATED);
 
         itemModelGenerator.register(ModItems.TILE_TROWEL, Models.HANDHELD);
     }
--- src/main/java/com/proctoredgames/tiled/util/TileColors.java
+++ src/main/java/com/proctoredgames/tiled/util/TileColors.java
@@ -16,22 +16,22 @@ public class TileColors {
     private static final Map<Item, Item> SMALL_TILE_TO_CONCRETE = new HashMap<>();
 
     static {
-        register(Items.WHITE_CONCRETE, ModItems.WHITE_TILE, ModItems.SMALL_WHITE_TILE);
-        register(Items.ORANGE_CONCRETE, ModItems.ORANGE_TILE, ModItems.SMALL_ORANGE_TILE);
-        register(Items.MAGENTA_CONCRETE, ModItems.MAGENTA_TILE, ModItems.SMALL_MAGENTA_TILE);
-        register(Items.LIGHT_BLUE_CONCRETE, ModItems.LIGHT_BLUE_TILE, ModItems.SMALL_LIGHT_BLUE_TILE);
-        register(Items.YELLOW_CONCRETE, ModItems.YELLOW_TILE, ModItems.SMALL_YELLOW_TILE);
-        register(Items.LIME_CONCRETE, ModItems.LIME_TILE, ModItems.SMALL_LIME_TILE);
-        register(Items.PINK_CONCRETE, ModItems.PINK_TILE, ModItems.SMALL_PINK_TILE);
-        register(Items.GRAY_CONCRETE, ModItems.GRAY_TILE, ModItems.SMALL_GRAY_TILE);
-        register(Items.LIGHT_GRAY_CONCRETE, ModItems.LIGHT_GRAY_TILE, ModItems.SMALL_LIGHT_GRAY_TILE);
-        register(Items.CYAN_CONCRETE, ModItems.CYAN_TILE, ModItems.SMALL_CYAN_TILE);
-        register(Items.PURPLE_CONCRETE, ModItems.PURPLE_TILE, ModItems.SMALL_PURPLE_TILE);
-        register(Items.BLUE_CONCRETE, ModItems.BLUE_TILE, ModItems.SMALL_BLUE_TILE);
-        register(Items.BROWN_CONCRETE, ModItems.BROWN_TILE, ModItems.SMALL_BROWN_TILE);
-        register(Items.GREEN_CONCRETE, ModItems.GREEN_TILE, ModItems.SMALL_GREEN_TILE);
-        register(Items.RED_CONCRETE, ModItems.RED_TILE, ModItems.SMALL_RED_TILE);
-        register(Items.BLACK_CONCRETE, ModItems.BLACK_TILE, ModItems.SMALL_BLACK_TILE);
+        register(Items.WHITE_CONCRETE, ModItems.WHITE_CONCRETE_TILE, ModItems.SMALL_WHITE_CONCRETE_TILE);
+        register(Items.ORANGE_CONCRETE, ModItems.ORANGE_CONCRETE_TILE, ModItems.SMALL_ORANGE_CONCRETE_TILE);
+        register(Items.MAGENTA_CONCRETE, ModItems.MAGENTA_CONCRETE_TILE, ModItems.SMALL_MAGENTA_CONCRETE_TILE);
+        register(Items.LIGHT_BLUE_CONCRETE, ModItems.LIGHT_BLUE_CONCRETE_TILE, ModItems.SMALL_LIGHT_BLUE_CONCRETE_TILE);
+        register(Items.YELLOW_CONCRETE, ModItems.YELLOW_CONCRETE_TILE, ModItems.SMALL_YELLOW_CONCRETE_TILE);
+        register(Items.LIME_CONCRETE, ModItems.LIME_CONCRETE_TILE, ModItems.SMALL_LIME_CONCRETE_TILE);
+        register(Items.PINK_CONCRETE, ModItems.PINK_CONCRETE_TILE, ModItems.SMALL_PINK_CONCRETE_TILE);
+        register(Items.GRAY_CONCRETE, ModItems.GRAY_CONCRETE_TILE, ModItems.SMALL_GRAY_CONCRETE_TILE);
+        register(Items.LIGHT_GRAY_CONCRETE, ModItems.LIGHT_GRAY_CONCRETE_TILE, ModItems.SMALL_LIGHT_GRAY_CONCRETE_TILE);
+        register(Items.CYAN_CONCRETE, ModItems.CYAN_CONCRETE_TILE, ModItems.SMALL_CYAN_CONCRETE_TILE);
+        register(Items.PURPLE_CONCRETE, ModItems.PURPLE_CONCRETE_TILE, ModItems.SMALL_PURPLE_CONCRETE_TILE);
+        register(Items.BLUE_CONCRETE, ModItems.BLUE_CONCRETE_TILE, ModItems.SMALL_BLUE_CONCRETE_TILE);
+        register(Items.BROWN_CONCRETE, ModItems.BROWN_CONCRETE_TILE, ModItems.SMALL_BROWN_CONCRETE_TILE);
+        register(Items.GREEN_CONCRETE, ModItems.GREEN_CONCRETE_TILE, ModItems.SMALL_GREEN_CONCRETE_TILE);
+        register(Items.RED_CONCRETE, ModItems.RED_CONCRETE_TILE, ModItems.SMALL_RED_CONCRETE_TILE);
+        register(Items.BLACK_CONCRETE, ModItems.BLACK_CONCRETE_TILE, ModItems.SMALL_BLACK_CONCRETE_TILE);
     }
 
     private static void register(Item concrete, Item tile, Item smallTile) {
